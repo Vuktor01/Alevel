0760c1793b38158ae961d9d99b40eff94e1e54a4
Vuktor01 <133669369+Vuktor01@users.noreply.github.com>
2023-11-09 16:12:43 +0300

    Add salad homework

---
+++ HomeworkVegetableSaladSolution/VegetableSaladConsoleApp/RootVegetable.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace VegetableSaladConsoleApp
+{
+    public class RootVegetable : Vegetable
+    {
+        public bool HasEdibleRoots { get; set; }
+
+        public RootVegetable(string name, double weight, double caloriesPer100g, bool hasEdibleRoots)
+            : base(name, weight, caloriesPer100g)
+        {
+            HasEdibleRoots = hasEdibleRoots;
+        }
+
+        public override double CalculateCalories()
+        {
+            double baseCalories = base.CalculateCalories();
+            return HasEdibleRoots ? baseCalories * 1.5 : baseCalories;
+        }
+    }
+}
---
+++ HomeworkVegetableSaladSolution/VegetableSaladConsoleApp/Salad.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace VegetableSaladConsoleApp
+{
+    public class Salad
+    {
+        private Vegetable[] vegetables = new Vegetable[0];
+        private int count = 0;
+
+        public void AddVegetable(Vegetable vegetable)
+        {
+            Array.Resize(ref vegetables, count + 1);
+            vegetables[count] = vegetable;
+            count++;
+        }
+
+        public double GetTotalCalories()
+        {
+            double totalCalories = 0;
+            foreach (var vegetable in vegetables)
+            {
+                totalCalories += vegetable.CalculateCalories();
+            }
+            return totalCalories;
+        }
+    }
+}

c54debb8b38223ea8e709db117138c27eb28ab4d
Vuktor01 <133669369+Vuktor01@users.noreply.github.com>
2023-12-08 21:17:38 +0300

    Homework3.4Add

---
+++ Homework3.4/Homework3.4 ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        Calculator calculator = new Calculator();
+
+
+        calculator.Calculate += PlusNumbers;
+        calculator.Calculate += MultiplyNumbers;
+
+
+        int result = CalculateWithExceptionHandler(calculator, 5, 3);
+        Console.WriteLine("Результат: " + result);
+    }
+
+    public static int CalculateWithExceptionHandler(Calculator calculator, int a, int b)
+    {
+        try
+        {
+            return ExecuteCalculation(calculator, a, b);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+            return 0;
+        }
+    }
+
+    public static int ExecuteCalculation(Calculator calculator, int a, int b)
+    {
+        return calculator.Sum(a, b);
+    }
+
+    public static int PlusNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int MultiplyNumbers(int a, int b)
+    {
+        return a * b;
+    }
+}
+public delegate int CalculateDelegate(int a, int b);
+
+public class Calculator
+{
+    public event CalculateDelegate Calculate;
+
+    public int Sum(int a, int b)
+    {
+        if (Calculate != null)
+            return Calculate(a, b);
+        return 0;
+    }
+}
---
+++ Homework3.4/LinQConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Contact
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class LinqExample
+{
+    public static void Main()
+    {
+        List<Contact> contacts = new List<Contact>
+        {
+            new Contact { Name = "Alice", Age = 25 },
+            new Contact { Name = "Bob", Age = 30 },
+            new Contact { Name = "Charlie", Age = 22 }
+        };
+
+        Contact firstContact = contacts.FirstOrDefault();
+        Console.WriteLine("Первый контакт: " + firstContact?.Name);
+
+        var adults = contacts.Where(c => c.Age >= 25);
+        Console.WriteLine("Взрослые:");
+        foreach (var adult in adults)
+        {
+            Console.WriteLine(adult.Name);
+        }
+
+        var names = contacts.Select(c => c.Name);
+        Console.WriteLine("Имена контактов:");
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        var orderedContacts = contacts.OrderBy(c => c.Age);
+        Console.WriteLine("Контакты по возрасту:");
+        foreach (var contact in orderedContacts)
+        {
+            Console.WriteLine($"{contact.Name}, Возраст: {contact.Age}");
+        }
+
+        int count = contacts.Count();
+        Console.WriteLine("Общее количество контактов: " + count);
+
+        bool hasAdults = contacts.Any(c => c.Age >= 18);
+        Console.WriteLine("Есть ли взрослые: " + hasAdults);
+    }
+}

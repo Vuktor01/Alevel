1352e449b90d4474eeb2b6572bd03eaf8a60611d
Vuktor01 <133669369+Vuktor01@users.noreply.github.com>
2023-12-12 16:29:45 +0300

    3.7 HomeworkAdd

---
+++ Homework3.7/Homework3.7ConsoleApp/AppConfigHelper.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+public static class AppConfigHelper
+{
+    private const string AppConfigFileName = "appconfig.json";
+    private static AppConfig appConfig;
+
+    static AppConfigHelper()
+    {
+        LoadAppConfig();
+    }
+
+    private static void LoadAppConfig()
+    {
+        string configJson = File.ReadAllText(AppConfigFileName);
+        appConfig = JsonConvert.DeserializeObject<AppConfig>(configJson);
+    }
+
+    public static int GetBackupFrequency()
+    {
+        return appConfig?.BackupFrequency ?? 0;
+    }
+}
---
+++ Homework3.7/Homework3.7ConsoleApp/Logger.cs
@@ -0,0 +1,45 @@
+﻿public class Logger
+{
+    public event EventHandler BackupRequired;
+
+    private const string LogFilePath = "log.txt";
+    private const string BackupFolderPath = "Backup";
+
+    private int logCount;
+
+    public Logger()
+    {
+        logCount = 0;
+    }
+
+    public async Task LogAsync(string message)
+    {
+        logCount++;
+
+        if (logCount % AppConfigHelper.GetBackupFrequency() == 0)
+        {
+            Backup();
+            OnBackupRequired();
+        }
+
+        using (StreamWriter writer = new StreamWriter(LogFilePath, true))
+        {
+            await writer.WriteLineAsync($"{DateTime.Now} - {message}");
+        }
+    }
+
+    private void Backup()
+    {
+        string backupFileName = $"Backup_{DateTime.Now:yyyyMMddHHmmss}.txt";
+        string backupFilePath = Path.Combine(BackupFolderPath, backupFileName);
+
+        Directory.CreateDirectory(BackupFolderPath);
+
+        File.Copy(LogFilePath, backupFilePath);
+    }
+
+    private void OnBackupRequired()
+    {
+        BackupRequired?.Invoke(this, EventArgs.Empty);
+    }
+}
